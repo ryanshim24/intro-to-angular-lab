6ad25ba3d45263304eab10c142029ad9b35d9479
Ryan Shim <ryanshim92@gmail.com>
2014-12-02 16:34:44 -0800

    final solution

--- app/books_controller.js
+++ app/books_controller.js
@@ -5,5 +5,28 @@ bookly.controller('BooksController', function($scope) {
   $scope.books = books;
 
   //Code to manage cart goes here
+  $scope.total = 0;
+  $scope.items = [];
+
+  // add to that cart
+  $scope.addCart = function(book) {
+    $scope.items.push(book);
+    console.log($scope.items);
+    $scope.total += book.price;
+  };
+
+  //delete whole cart
+  $scope.deleteItem = function() {
+    $scope.items = [];
+    $scope.total = 0;
+  };
+
+  // delete individual items
+  $scope.deleteInd = function(item) {
+    console.log(item);
+    var select = $scope.items.indexOf(item);
+    $scope.items.splice(select, 1);
+    $scope.total -= item.price;
+  };
 
 });
